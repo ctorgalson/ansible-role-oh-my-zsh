8dd4a241775a672e9d7b7b12b6e657f87d0574a5
Christopher Torgalson <manager@bedlamhotel.com>
2018-08-01 21:36:29 +0200

    Adds tests to default scenario

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,13 +2,54 @@ import os
 
 import testinfra.utils.ansible_runner
 
+import pytest
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize("package", [
+    "zsh",
+])
+def test_packages(host, package):
+    p = host.package(package)
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert p.is_installed
+
+
+@pytest.mark.parametrize("name", [
+    "lorem",
+    "ipsum",
+])
+def test_user_shell(host, name):
+    u = host.user(name)
+
+    assert 'zsh' in u.shell
+
+
+@pytest.mark.parametrize("name", [
+    "lorem",
+    "ipsum",
+])
+def test_oh_my_zsh_install(host, name):
+    d = host.file("/home/{0}/.oh-my-zsh".format(name))
+
+    assert d.exists and d.is_directory
+
+
+@pytest.mark.parametrize("name", [
+    "lorem",
+])
+def test_zshrc_create(host, name):
+    f = host.file("/home/{0}/.zshrc".format(name))
+
+    assert f.exists and f.is_file
+
+
+@pytest.mark.parametrize("name", [
+    "ipsum",
+])
+def test_zshrc_alter(host, name):
+    f = host.file("/home/{0}/.zshrc".format(name))
+
+    assert "ls -AF" in f.content_string
